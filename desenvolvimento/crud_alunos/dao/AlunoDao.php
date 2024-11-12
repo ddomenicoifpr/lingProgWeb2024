564337e1b330267987046133f4ad834eb4e7f6d2
Daniel Di Domenico <daniel.domenico@ifpr.edu.br>
2024-11-12 08:58:45 -0300

    Rotinas para atualizar o aluno

--- desenvolvimento/crud_alunos/dao/AlunoDao.php
+++ desenvolvimento/crud_alunos/dao/AlunoDao.php
@@ -29,6 +29,19 @@ class AlunoDao {
         $stm->execute(array($id));
     }
 
+    public function update(Aluno $aluno) {
+        $conn = Connection::getConnection();
+
+        $sql = "UPDATE alunos SET nome = ?, idade = ?,". 
+            " estrangeiro = ?, id_curso = ?".
+            " WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->execute([$aluno->getNome(), $aluno->getIdade(), 
+                        $aluno->getEstrangeiro(), $aluno->getCurso()->getId(),
+                        $aluno->getId()]);
+    }
+
     public function list() {
         $sql = "SELECT a.*, c.nome curso_nome, c.turno curso_turno
                 FROM alunos a
@@ -40,6 +53,30 @@ class AlunoDao {
 
         $alunos = $this->mapAlunos($result);
         return $alunos;
+    }
+
+    public function findById(int $id) {
+        $conn = Connection::getConnection();
+
+        $sql = "SELECT a.*, c.nome curso_nome, c.turno curso_turno
+                FROM alunos a
+                JOIN cursos c ON (c.id = a.id_curso)
+                WHERE a.id = ?";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$id]);
+        $result = $stmt->fetchAll();
+
+        //Criar o objeto Aluno
+        $alunos = $this->mapAlunos($result);
+
+        if(count($alunos) == 1)
+            return $alunos[0];
+        elseif(count($alunos) == 0)
+            return null;
+
+        die("AlunoDAO.findById - Erro: mais de um aluno".
+                " encontrado para o ID " . $id);
     }
 
     private function mapAlunos(array $result) {
